df2b3cb3d18a15b9bb8f6114e6a2e8d8ac30dca5
Matcast-ox <matcastilho03@gmail.com>
2026-03-24 10:08:13 -0300

    Aprendemos incremento e descremento

---
+++ I1DS-2026/aulas/12-lacoRepeticao.js
@@ -0,0 +1,7 @@
+// Laço de repetição - For (PARA)
+// O professor te colocou de castigo e pediu para você escrever mil vezes a frase
+// "Eu vou prestar atenção ás aulas e anotar tudo!"
+
+for (let i = 0; i < 1000; i++) {// i++ é == i = i+1
+   
+}
