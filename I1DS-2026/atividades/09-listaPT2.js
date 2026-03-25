fa3669dc539c671e1aaf6bac38731190c682ca18
Matcast-ox <matcastilho03@gmail.com>
2026-03-25 11:07:34 -0300

    salvo 1

--- I1DS-2026/atividades/09-listaPT2.js
+++ I1DS-2026/atividades/09-listaPT2.js
@@ -1,4 +1,31 @@
 //Lista de exercicios parte 2
 //Sabendo se um numero é positivo, negativo ou zero
 //definindo numeros
-let.console
+let numero1 = -50;
+let numero2 = 900;
+let numero3 = 0;
+
+if (numero1 > 0) {
+  console.log("numero1 positivo");
+} else if (numero1 < 0) {
+  console.log("numero1 negativo");
+} else {
+  console.log("numero1 é zero");
+}
+
+if (numero2 > 0) {
+  console.log("numero2 positivo");
+} else if (numero2 < 0) {
+  console.log("numero2 negativo");
+} else {
+  console.log("numero2 é zero");
+}
+
+if (numero3 > 0) {
+  console.log("numero3 positivo");
+} else if (numero3 < 0) {
+  console.log("numero3 negativo");
+} else {
+  console.log("numero3 é zero");
+}
+console.log("------------------------------------")
